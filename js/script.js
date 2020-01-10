3606a6aba54c3f3170dc591c0b80d6bc94909b6e
Maxine Hartnett <maxinehartnett@gmail.com>
2020-01-09 20:09:26 -0700

    I ripped out a bunch of random crap but added some way way worse crap aint that just the way

--- js/script.js
+++ js/script.js
@@ -2,24 +2,33 @@ $(document).ready(function(){
     var incr=0;
 
     function addDropdown(){
-        //var target=$(this).find(":selected").attr("data-target");
-        //var id=$(this).attr("id");
-        //$("div[id^='"+id+"']").hide();
-        //$("#"+id+"-"+target).show();
 
         var name = $("<div/>",{"text":'imamoron',"test":"name","class":"test"});
 
         var select = $("<select/>", {"name":"customname" , "class":"custom"+incr});
-        var option1 = $("<option/>", {"class":'option'+incr, "text":"testtext"+incr});
-        $("body").append(option1);  
 
-        var option2 = $("<option/>", {"class":'option'+incr, "text":"11testtext"+incr});
-        $("body").append(option2);
+        // this is like the worst code I've written in my life
+        // I'd like to take this opportunity to apologize to my mom and to god
+
+        if (incr==0){
+            var option1 = $("<option/>", {"class":'option'+incr, "text":"imgay"+incr});
+            $("body").append(option1);  
+
+            var option2 = $("<option/>", {"class":'option'+incr, "text":"11testtext"+incr});
+            $("body").append(option2);
+        }
+        else{
+            var option1 = $("<option/>", {"class":'option'+incr, "text":"testtext"+incr});
+            $("body").append(option1);  
+
+            var option2 = $("<option/>", {"class":'option'+incr, "text":"11testtext"+incr});
+            $("body").append(option2);
+        }
+        
 
         $('.option'+incr).wrapAll(select)
         $(".custom"+incr).wrap("<div class='cool'> </div>")
         
-        $("body").append("TEST");   
         incr++;
     }
 
